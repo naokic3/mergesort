10a10f8d06bb47361a244725c0e35ba179dcab33
Naoki Conning <naokic2@nycstudents.net>
2025-03-17 11:48:09 -0400

    edit

--- merge.java
+++ merge.java
@@ -1,79 +1,80 @@
 import java.util.stream.IntStream;
 
 public class merge{
-public static int[] mergesortM(int[] data) {
+public static int[] mergesortM(int[] data){
     int[] temp = new int[data.length];
     int m = 2;
     boolean osc = true;
-    
-    while (m < 2 * data.length) {
-        int num = (data.length + m - 1) / m;
-        final int mFinal = m;  // Need final variable for lambda
-        
-        if (osc) {
-            // Merge from data into temp
-            IntStream.range(0, num)
-                    .parallel()
-                    .forEach(i -> {
-                        int lo = i * mFinal;
-                        int mid = Math.min(lo + mFinal/2, data.length);
-                        int hi = Math.min(lo + mFinal, data.length);
-                        
-                        if (mid < hi) {
-                            merge(data, temp, lo, mid, hi);
-                        } else {
 
-                            for (int j = lo; j < hi; j++) {
-                                temp[j] = data[j];
-                            }
-                        }
-                    });
-        } else {
-            IntStream.range(0, num)
-                    .parallel()
-                    .forEach(i -> {
-                        int lo = i * mFinal;
-                        int mid = Math.min(lo + mFinal/2, data.length);
-                        int hi = Math.min(lo + mFinal, data.length);
-                        
-                        if (mid < hi) {
-                            merge(temp, data, lo, mid, hi);
-                        } else {
-                            for (int j = lo; j < hi; j++) {
-                                data[j] = temp[j];
-                            }
-                        }
-                    });
+    while(m < 2 * data.length){
+        int operations = (data.length + m - 1) / m;
+        final int mf = m;
+
+        if(osc){
+          IntStream.range(0, operations)
+            .parallel()
+            .forEach(i -> {
+              int lo = i * mf;
+              int split = Math.min(lo + mFinal/2, data.length);
+              int hi = Math.min(lo + mFinal, data.length);
+              if(split < hi){
+                merge(data, temp, lo, split, hi);
+              }else{
+                for(int b=lo; b < hi; b++){
+                  temp[b] = data[b];
+                }
+              }
+              });
+        }else{ //flip between the two arrays.
+          IntStream.range(0, operations)
+            .parallel()
+            .forEach(i -> {
+              int lo = i * mf;
+              int split = Math.min(lo + mFinal/2, data.length);
+              int hi = Math.min(lo + mFinal, data.length);
+              if(split < hi){
+                merge(data, temp, lo, split, hi);
+              }else{
+                for(int b=lo; b < hi; b++){
+                  temp[b] = data[b];
+                }
+              }
+              });
         }
-        
-        m *= 2;
+        m = m * 2;
         osc = !osc;
     }
-    
-    if (!osc) {
-        return temp;
-    } else {
-        return data;
+    if(osc){
+      return data;
+    }else{
+      return temp;
     }
 }
 
-public static void merge(int[] arr, int[] temp, int left, int mid, int right) {
-    int i = left;  
-    int j = mid;  
-    int k = left;   
-
-    while (i < mid && j < right) {
-        if (arr[i] <= arr[j]) {
-            temp[k++] = arr[i++];
-        } else {
-            temp[k++] = arr[j++];
+public static void merge(int[] arr, int[] temp, int lo, int split, int hi) {
+    int a = lo;
+    int b = split;
+    int c = lo;
+    while(a < split && b < hi){
+        if(arr[a] <= arr[b]){
+          temp[c] = arr[a];
+          a++;
+          c++;
+        }else{
+          temp[c] = arr[b];
+            c++;
+            b++;
         }
     }
-    while (i < mid) {
-        temp[k++] = arr[i++];
+    while(a < split) {
+        temp[c] = arr[a];
+        c++;
+        a++;
     }
-    while (j < right) {
-        temp[k++] = arr[j++];
+    while (b < hi) {
+        temp[c] = arr[b];
+        c++;
+        b++
     }
 }
 
